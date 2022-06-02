8b23a36070d0de540f4bf639c9af6b9ebe4a45bc
ocean272 <ocean272@hotmail.com>
2022-06-02 16:36:16 +0800

    added filter for  3 dropdown fields

--- supper_app/src/components/SearchForm.js
+++ supper_app/src/components/SearchForm.js
@@ -31,75 +31,70 @@ const SearchForm = () => {
 
   return (
     <section>
-      <div className="container">
-        <h4 className="mb-3 text-center mt-4">Search Restaruant location</h4>
-        <div className="row mt-3">
-          <form>
-            <h3>Location</h3>
-            <select
-                onClick={searchRecords}
-                onChange={handleChange}> 
-              <option value="N/A">Please choose</option>
-              <option value="North">North</option>
-              <option value="South">South</option>
-              <option value="East">East</option>
-              <option value="West">West</option>
-              <option value="Central">Central</option>
-            </select>
-          </form>
+        <form>
+          <p>Location</p>
+          <select
+              onClick={searchRecords}
+              onChange={handleChange}> 
+            <option value="N/A">Please choose</option>
+            <option value="North">North</option>
+            <option value="South">South</option>
+            <option value="East">East</option>
+            <option value="West">West</option>
+            <option value="Central">Central</option>
+          </select>
+        </form>
 
-          <form>
-            <h3>Cuisine offered</h3>
-            <select
-                onClick={searchRecords}
-                onChange={handleChange1}> 
-              <option value="N/A">Please choose</option>
-              <option value="Western">Western</option>
-              <option value="Muslim">Muslim</option>
-              <option value="Indian">Indian</option>
-              <option value="Chinese">Chinese</option>
-              <option value="Japanese">Japanese</option>
-              <option value="Korean">Korean</option>
-            </select>
-          </form>
+        <form>
+          <p>Cuisine offered</p>
+          <select
+              onClick={searchRecords}
+              onChange={handleChange1}> 
+            <option value="N/A">Please choose</option>
+            <option value="Western">Western</option>
+            <option value="Muslim">Muslim</option>
+            <option value="Indian">Indian</option>
+            <option value="Chinese">Chinese</option>
+            <option value="Japanese">Japanese</option>
+            <option value="Korean">Korean</option>
+          </select>
+        </form>
 
-          <form>
-            <h3>Price range</h3>
-            <select
-                onClick={searchRecords}
-                onChange={handleChange2}> 
-              <option value="N/A">Please choose</option>
-              <option value="$">$</option>
-              <option value="$$">$$</option>
-              <option value="$$$">$$$</option>
-              <option value="$$$$">$$$$</option>
-              <option value="$$$$$">$$$$$</option>
-            </select>
-          </form>
+        <form>
+          <p>Price range</p>
+          <select
+              onClick={searchRecords}
+              onChange={handleChange2}> 
+            <option value="N/A">Please choose</option>
+            <option value="$">$</option>
+            <option value="$$">$$</option>
+            <option value="$$$">$$$</option>
+            <option value="$$$$">$$$$</option>
+            <option value="$$$$$">$$$$$</option>
+          </select>
+        </form>
         <table className="table table-hover  table-striped table-bordered ml-4 ">
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Address</th>
-                <th>located_at</th>
-                <th>Cuisine</th>
-                <th>Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            {record.map((location) => (
-                <tr key={location.id} >
-                <td>{location.name}</td>
-                <td>{location.address}</td>
-                <td>{location.located_at}</td>
-                <td>{location.cuisineId}</td>
-                <td>{location.priceId}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>     
-        </div>
-      </div>
+          <thead>
+          <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>located_at</th>
+              <th>Cuisine</th>
+              <th>Price</th>
+          </tr>
+          </thead>
+          <tbody>
+          {record.map((location) => (
+              <tr key={location.id} >
+              <td>{location.name}</td>
+              <td>{location.address}</td>
+              <td>{location.located_at}</td>
+              <td>{location.cuisineId}</td>
+              <td>{location.priceId}</td>
+              </tr>
+          ))}
+          </tbody>
+      </table>     
     </section>
   );
 };
